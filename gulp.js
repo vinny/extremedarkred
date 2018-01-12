1f75328169d36d9fedbd496f4adbb465f07cbac0
Marcus Vinicius <vinny@suportephpbb.com.br>
2018-01-12 04:52:11 -0200

    Create gulp.js

---
+++ gulp.js
@@ -0,0 +1,27 @@
+var css = [
+	'./extremedarkred/theme/stylesheet.css'
+];
+ 
+var gulp = require('gulp');
+ 
+var watch = require('gulp-watch');
+ 
+var cssmin = require("gulp-cssmin");
+ 
+var concat = require("gulp-concat");
+ 
+var stripCssComments = require('gulp-strip-css-comments');
+ 
+gulp.task('minify-css', function(){
+	gulp.src(css)
+	.pipe(concat('stylesheet.min.css'))
+	.pipe(stripCssComments({all: true}))
+	.pipe(cssmin())
+	.pipe(gulp.dest('./extremedarkred/theme/'));
+});
+ 
+gulp.task('default','minify-css');
+ 
+gulp.task('watch', function() {
+	gulp.watch(css, ['minify-css']);
+});
